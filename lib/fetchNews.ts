c7497f5022264de45c41623eebb3d4d10fd2ac01
Lepha <lepharamchiary@gmail.com>
2023-05-22 23:30:05 +0530

    new page for fetch the news datas

---
+++ lib/fetchNews.ts
@@ -0,0 +1,13 @@
+const fetchNews = async (
+  category?: Category | string,
+  keywords?: string,
+  isDynamic?: boolean
+) => {
+  // GarphQL query
+  // Fetch function with Next.js 13 caching..
+  // Sort function by image vs not image present
+  // return res
+};
+
+
+export default fetchNews;
