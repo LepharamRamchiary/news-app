c2940c5235219116bd5420644a94932141a6f7e0
Lepha <lepharamchiary@gmail.com>
2023-05-22 23:26:45 +0530

    modification

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-
 import "@/styles/globals.css";
 import Head from "./head";
 import Header from "./Header";
@@ -9,15 +8,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
+    // dark:bg-zinc-900
     <html>
       <Head />
-      <body>
+      <body className="bg-gray-100  transition-all duration-700">
         <Header />
-        <div>
-        {children}
-        </div>
+        <div className="max-w-6xl mx-auto" >{children}</div>
       </body>
     </html>
   );
 }
-
