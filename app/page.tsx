682a6c8b4eb439033001fb4873d7e4b4151cce88
Lepha <lepharamchiary@gmail.com>
2023-05-23 23:47:13 +0530

    .

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { categories } from "../constants";
 
 async function Homepage() {
   // fetch the news data
-  const news: NewsResponse = await fetchNews(categories.join(","));
+  // const news: NewsResponse = await fetchNews(categories.join(","));
   return <div>{/* NewsList news */}</div>;
 }
 
